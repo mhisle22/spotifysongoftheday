8d7a1f853e8c72d0dabf2de7e093d775c566c2f3
Mark Hisle <mhisle22@gmail.com>
2021-06-01 13:22:07 -0400

    fix package requirement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-import version from '../../package.json';
+import pkg from '../../package.json';
 
 export const environment = {
   production: false,
-  version: version.version
+  version: pkg.version
 };
 
 /*
