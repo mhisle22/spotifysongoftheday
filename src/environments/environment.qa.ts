685ee44e6ff3dbb755e96ee939374835b43843aa
Mark Hisle <mhisle22@gmail.com>
2021-09-23 11:00:29 -0400

    add qa file

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -42,6 +42,52 @@ export const environment: EnvironmentInterface = {
     artist: 'I DONT KNOW HOW BUT THEY FOUND ME',
     image_link: 'https://i.scdn.co/image/ab67616d0000b273e9774ee3932efbc4c3d68e6b',
     uri: '37mfTcSlX60JtAvAETytGs'
-  } ]
+  } ],
+
+  playlistSongs: [{
+    username: 'fakeUser1234', // PK
+    URI: '4Y1igB1GDncJvt7ezbV3cW', // SortKey
+    artist: 'Thom Yorke',
+    link: 'https://open.spotify.com/track/4Y1igB1GDncJvt7ezbV3cW',
+    song: 'Suspirium',
+    timestamp: new Date('2021-09-21'),
+    position: 0
+  },
+  {
+    username: 'fakeUser1234', // PK
+    URI: '1R8kvV2AgNPCA2Pp4Im1Ao', // SortKey
+    artist: 'Jack Staubers Micropop',
+    link: 'https://open.spotify.com/track/1R8kvV2AgNPCA2Pp4Im1Ao',
+    song: 'Baby Hotline',
+    timestamp: new Date('2021-09-22'),
+    position: 0
+  },
+  {
+    username: 'fakeUser1234', // PK
+    URI: '2QwObYJWyJTiozvs0RI7CF', // SortKey
+    artist: 'Radiohead',
+    link: 'https://open.spotify.com/track/2QwObYJWyJTiozvs0RI7CF',
+    song: 'Street Spirit (Fade Out)',
+    timestamp: new Date('2021-09-23'),
+    position: 0
+  },
+  {
+    username: 'fakeUser1234', // PK
+    URI: '4nlvKIIetOWGIMyhjQXgOZ', // SortKey
+    artist: 'Flume',
+    link: 'https://open.spotify.com/track/4nlvKIIetOWGIMyhjQXgOZ',
+    song: 'The Difference',
+    timestamp: new Date('2021-09-24'),
+    position: 0
+  },
+  {
+    username: 'fakeUser1234', // PK
+    URI: '37mfTcSlX60JtAvAETytGs', // SortKey
+    artist: 'I DONT KNOW HOW BUT THEY FOUND ME',
+    link: 'https://open.spotify.com/track/37mfTcSlX60JtAvAETytGs',
+    song: 'Choke',
+    timestamp: new Date('2021-09-24 12:12:12'),
+    position: 0
+  }]
 
 };
